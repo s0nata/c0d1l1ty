21cf5e4884b4a6e75e18c7e8c084704b4e6fde3c
the-sentinel-ua <the.sentinel.ua@gmail.com>
2014-05-13 23:15:32 +0200

    Improved the solution to the GenomicRangeQuery problem

--- lessons/03_prefix_sums/genomic_range_query.cpp
+++ lessons/03_prefix_sums/genomic_range_query.cpp
@@ -1,6 +1,6 @@
 /*
  * Source: https://codility.com/demo/take-sample-test/genomic_range_query
- * Result: https://codility.com/demo/results/demoQ98URJ-QT7/
+ * Result: 100/100 @ https://codility.com/demo/results/demoYM2HWJ-MEH/
  * 
  * A non-empty zero-indexed string S is given. String S consists
  * of N characters from the set of upper-case English letters A,
@@ -79,58 +79,41 @@
  */
 
 #include <vector>
-
 vector<int> solution(string &S, vector<int> &P, vector<int> &Q) {
-    int N = S.size();
+    int N = S.length();
     int M = P.size();
+    int ACGT = 4, A = 0, C = 1, G = 2, T = 3;
     
-    int A = 0;
-    int C = 1;
-    int G = 2;
-    int T = 3;
-    
-    // pass 1, aux data structure, O(N) time
-    // partial-sums-inspired
-    vector<int> acgt(4,0);
-    vector< vector<int> > aux(N,acgt);
+    vector<int> acgt(ACGT,0);
+    vector< vector<int> > nucl_so_far(N+1,acgt);
     
     for(int i = 0; i < N; i++) {
-        if (i > 0) {
-            for(int k = 0; k < 4; k++) {
-                aux[i][k] = aux[i-1][k];
-            }
-        }
-        if (S[i] == 'A') aux[i][A]++;
-        if (S[i] == 'C') aux[i][C]++;
-        if (S[i] == 'G') aux[i][G]++;
-        if (S[i] == 'T') aux[i][T]++;
+        for(int j = 0; j < ACGT; j++)
+            nucl_so_far[i+1][j] = nucl_so_far[i][j];
+            
+        if (S[i] == 'A') nucl_so_far[i+1][A]++;
+        if (S[i] == 'C') nucl_so_far[i+1][C]++;
+        if (S[i] == 'G') nucl_so_far[i+1][G]++;
+        if (S[i] == 'T') nucl_so_far[i+1][T]++;
     }
     
-    //pass 2, O(M) time
-    vector<int> res(M,0);
+    vector<int> min_impacts(M,0);
     
-    for(int j = 0; j < M; j++) {
-        if (P[j] == Q[j]) {
-            if (S[P[j]] == 'A') res[j] = A + 1;
-            if (S[P[j]] == 'C') res[j] = C + 1;
-            if (S[P[j]] == 'G') res[j] = G + 1;
-            if (S[P[j]] == 'T') res[j] = T + 1;
+    for(int i = 0; i < M; i++) {
+        if (nucl_so_far[Q[i]+1][A] - nucl_so_far[P[i]][A] > 0) {
+            min_impacts[i] = A + 1;
         }
-        else {
-            if (aux[Q[j]][A] - aux[P[j]][A] > 0) {
-                res[j] = A + 1;
-            }
-            else if (aux[Q[j]][C] - aux[P[j]][C] > 0) {
-                res[j] = C + 1;
-            }
-            else if (aux[Q[j]][G] - aux[P[j]][G] > 0) {
-                res[j] = G + 1;
-            }
-            else {
-                res[j] = T + 1;
-            }
+        else if (nucl_so_far[Q[i]+1][C] - nucl_so_far[P[i]][C] > 0) {
+            min_impacts[i] = C + 1;
+        }
+        else if (nucl_so_far[Q[i]+1][G] - nucl_so_far[P[i]][G] > 0) {
+            min_impacts[i] = G + 1;
+        }
+        else if (nucl_so_far[Q[i]+1][T] - nucl_so_far[P[i]][T] > 0) {
+            min_impacts[i] = T + 1;
         }
     }
     
-    return res;
+    return min_impacts;
 }
+
