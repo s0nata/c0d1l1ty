8bf17dc3abab640b158afd7c793fb808ae59d958
the-sentinel-ua <the.sentinel.ua@gmail.com>
2014-06-07 22:39:12 +0200

    One more chunk of the sample code for the lesson 9

---
+++ lessons/09_sieve_of_eratosthenes/factorization.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# number factorization algorithm based on the sieve of Eratosthenes
+# from https://codility.com/media/train/9-Sieve.pdf
+
+import sys
+
+def sieve_int(n):
+    sieve = [0] * (n + 1)
+    sieve[0] = 0    # 1
+    sieve[1] = 0    # 2
+
+    i = 2
+    while( i * i < n):
+        if (sieve[i] == 0):
+            j = i * i
+            while(j <= n):
+                if (sieve[j] == 0):
+                    sieve[j] =  i;
+                j += i
+        i += 1
+
+    return sieve
+    
+def factorization(n,divisors):
+    prime_factors = []
+    
+    while(divisors[n] > 0):
+        prime_factors += [divisors[n]]
+        n /= divisors[n]
+        
+    prime_factors += [n]
+    
+    return prime_factors
+    
+n = int(sys.argv[1])
+tmp = sieve_int(n)
+primes = factorization(n,tmp)
+
+print "Here are the prime factors of %s:" % n
+for i in primes:
+    print i
