fc49775dc8b6def11497e2f734f44912d5a2ea73
the-sentinel-ua <the.sentinel.ua@gmail.com>
2014-08-17 12:22:27 +0200

    brute-force solution to the CountNonDivisible problem

--- lessons/09_sieve_of_eratosthenes/count_non_divisible.cpp
+++ lessons/09_sieve_of_eratosthenes/count_non_divisible.cpp
@@ -1,123 +1,44 @@
 /*
  * Source: https://codility.com/demo/take-sample-test/count_non_divisible
- * Result:  22/100
+ * Result:  66/100 (correctness 100, performance 25 -- O(N^2))
  */
-
-// improving my prev. solution from https://codility.com/demo/results/demoKP3WWY-KFV/
-
-#include<vector>
-#include<algorithm>    //max element
-#include<iostream>
+ 
+ #include <algorithm> // max_element()
 
 vector<int> solution(vector<int> &A) {
     int N = A.size();
     
-    if (N == 1) {
-        vector<int> not_divisors(1,0);
-        return not_divisors;
-    }
-    
-    //find the actual max element in A
-    int maxA = *max_element(A.begin(),A.end());
-    cout << "max el " << maxA << endl;
-    
-    // count how many times each element is met in A
-    vector<int> occurences(maxA+1,0);
-    
-    for(int i = 0; i < N; i++) {
-        occurences[A[i]]++;
-    }
-    
-    cout <<  "occurences\n";
-    for (int i = 0; i < occurences.size(); i++) cout << occurences[i] << "  ";
-    cout << endl;
-    
-    // make sort of eratosthenes sieve for possible
-    // max element of A (2*N from the task info)
-    
-    vector<int> sieve(maxA+1,0);
-    int p = 2, q;
-    while ( p * p < maxA ) {
-        if (sieve[p] == 0) {
-            q = p * p;
-            while (q <= maxA) {
-                if (sieve[q] == 0) {
-                    sieve[q] = p;
-                }
-                q += p;
-            }
-        }
-        p++;
-    }
-    
-    // actual work
-    vector<int> not_divisors(N,N);
-    vector<int> factors;
-    
-    for (int i = 0; i < N; i++) {
-        factors.clear();
-        if (A[i] > 1) {
-            not_divisors[i] -= occurences[1]; // number of "1" in A
-        }
-        not_divisors[i] -= occurences[A[i]]; //number of self-occurences
-        //here fill the "factors" vector
-        //but it may contain duplicates
-        //and it contains only half of the divisors
-        int M = A[i];
-        while (sieve[M] > 0) {
-            factors.push_back(sieve[M]);
-            M /= sieve[M];
-        }
-        factors.push_back(M);
-        //so here more factors
-        // how to do it in a correct way?
-        int T = factors.size();
-        for (int t= 0; t < T;t++) {
-            for (int u = t + 1; u < T;u++ ){
-                factors.push_back(factors[t]*factors[u]);
-            }
-        }
-        
-        cout << "current A[" << i <<"] " << A[i];
-        cout <<  "\n  factors\t\t";
-        for (int z = 0; z < factors.size(); z++) cout << factors[z] << "  ";
-        cout << endl;
-        
-        for (unsigned int j = 0; j < factors.size();j++) {
-            //not prime number
-            //here somehow manage duplicates in factors aux array
-            //so not to substract twice for same thing
-            if (factors[j] != A[i]) {
-                not_divisors[i] -= occurences[factors[j]];
-            }
-        }
-        
-        cout <<  "  non-divisors\t\t " << not_divisors[i]<<endl;
-    }
-    
-    return not_divisors;
-}
+    //count occurences of each element in A
+    int A_max = *max_element(A.begin(),A.end());
 
-/* Your test case [24 15 4 8 7] : NO RUNTIME ERRORS 
- * (returned value: [1, 4, 4, 1, 4] )
- * Output:
- *
- * max el 24
- * occurences
- * 0  0  0  0  1  0  0  1  1  0  0  0  0  0  0  1  0  0  0  0  0  0  0  0  1  
- * current A[0] 24
- *   factors		2  2  2  3  4  4  6  4  6  6  
- *   non-divisors		 1
- * current A[1] 15
- *   factors		3  5  15  
- *   non-divisors		 4
- * current A[2] 4
- *   factors		2  2  4  
- *   non-divisors		 4
- * current A[3] 8
- *   factors		2  2  2  4  4  4  
- *   non-divisors		 1
- * current A[4] 7
- *   factors		7  
- *   non-divisors		 4
-*/
+    vector<int> Atimes(A_max+1,0);
+    for(int i = 0; i < N; i++) Atimes[A[i]]++;
+    
+    //bf approach pass 1
+    vector<int> non_divs(A_max+1, 0);
+    for(int i = A_max; i > 0; i--) {
+        if (Atimes[i]> 0) {
+            for (int j = i-1; j > 0; j--) {
+                if (i % j > 0) {
+                    non_divs[i] += Atimes[j];
+                }
+            }
+        }
+    }
+
+    //bf approach pass 2
+    for (int i = 0; i < A_max +1 ; i++) {
+        if (Atimes[i] > 0) {
+            for (int j = i + 1; j < A_max+1; j++) {
+                non_divs[i] += Atimes[j];
+            }
+        }
+    }
+    
+    vector<int> res(N,0);
+    for(int i = 0; i < N; i++) {
+        res[i] = non_divs[A[i]];
+    }
+
+    return res;
+}
